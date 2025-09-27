69fee0ef28c3389d4336735f33e526099cbd5beb
Queency Delena <queencydelena@gmail.com>
2025-09-28 01:12:30 +0800

    test: initial testing setup

---
+++ GamifyMyLifeAPITests/UnitTest/MappersTests.cs
@@ -0,0 +1,25 @@
+using GamifyMyLifeAPI.Mappers;
+using GamifyMyLifeAPI.Dtos;
+using GamifyMyLifeAPI.DomainModels;
+using GamifyMyLifeAPI.Entities;
+namespace GamifyMyLifeAPITests.UnitTests
+{
+    public class MappersTests
+    {
+        
+        [Fact]        
+        public void CategoryMapper_ToDomain_ValidData_ReturnsCorrectObject()
+        {            
+        }
+
+        [Fact]
+        public void CategoryMapper_ToDomain_InvalidData_ReturnsNull()
+        {
+        }
+
+        [Fact]
+        public void CategoryMapper_ToEntity_ValidData_NotNull()
+        {
+        }
+    }
+}
